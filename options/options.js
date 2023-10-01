fc96386940276d627526fb6443c87cc378d8ded0
Crowdbotics <team@crowdbotics.com>
2023-10-01 22:08:11 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "jolly-rain-43696",
+  url: "https://jolly-rain-43696.botics.co",
+  api: "https://jolly-rain-43696.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
